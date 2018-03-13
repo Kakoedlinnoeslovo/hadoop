9c4af59e5b24f2a37939dc1d2a0350203d3a78c2
Roman Degtyarev <romandegtyarev@MacBook-Pro-Roman.local>
2018-03-13 04:41:10 +0300

    Должно все хорошо быть

--- hw1/src/main/java/inputformat/DocReader2.java
+++ hw1/src/main/java/inputformat/DocReader2.java
@@ -1,5 +1,6 @@
 package inputformat;
 
+import com.google.common.io.LittleEndianDataInputStream;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputSplit;
@@ -12,14 +13,28 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.io.IOUtils;
 
+import java.io.EOFException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.DataFormatException;
+import java.util.zip.Inflater;
 
 public class DocReader2 extends RecordReader<LongWritable, Text>{
+    private long max_doc = 5000000;
 
     private FSDataInputStream inputIndex;
     private Text value;
     private long documentNumber;
     private long filesNumber;
+    List<Integer> index_array;
+    long start_file;
+    int doc_num;
+    long n_files;
+    FSDataInputStream input_file;
+
+    byte[] input_arr;
+    byte[] result;
 
     public void initialize(InputSplit split, TaskAttemptContext context) throws IOException {
         Configuration conf = context.getConfiguration();
@@ -27,15 +42,56 @@ public class DocReader2 extends RecordReader<LongWritable, Text>{
         Path path = fsplit.getPath();
         FileSystem fs = path.getFileSystem(conf);
         inputIndex = fs.open(path);
-        System.out.println(inputIndex);
+        //System.out.println(inputIndex);
+        prepare_index(fsplit, path, fs);
 
 
     };
 
-    public LongWritable getCurrentKey() throws IOException, InterruptedException{
-        return new LongWritable(0);
+    private void prepare_index(FileSplit fsplit,
+                               Path path,
+                               FileSystem fs) throws IOException {
+        index_array = read_index(inputIndex);
+        start_file = fsplit.getStart();
 
-    };
+        long offset = 0;
+        while (doc_num < start_file) {
+            offset += index_array.get(doc_num);
+            doc_num++;
+        }
+        n_files = fsplit.getLength();
+
+        if (max_doc < 0)
+            throw new IOException("max doc error");
+
+        input_file = fs.open(path);
+        input_file.seek(offset);
+
+        input_arr = new byte[(int) max_doc];
+        result = new byte[(int) max_doc * 20];//?
+    }
+
+    private static List<Integer> read_index(FSDataInputStream index_file) throws IOException {
+        int max_doc = 0;
+        LittleEndianDataInputStream in = new LittleEndianDataInputStream(index_file);
+        List<Integer> al = new ArrayList<>();
+        try {
+            while (true) {
+                int val = in.readInt();
+                if (val > max_doc)
+                    max_doc = val;
+                al.add(val);
+            }
+        } catch (EOFException ignored) {
+        }
+        return al;
+    }
+
+    @Override
+    public LongWritable getCurrentKey() {
+        //index_array = offset + length
+        return new LongWritable(index_array.get(doc_num - 1));
+    }
 
     public Text getCurrentValue() throws IOException {
 //            System.out.println(value);
@@ -49,10 +105,32 @@ public class DocReader2 extends RecordReader<LongWritable, Text>{
 
 
 
-    public boolean nextKeyValue() throws IOException, InterruptedException{
+    @Override
+    public boolean nextKeyValue() throws IOException {
+        if (doc_num >= n_files)
+            return false;
 
+//            System.out.println("Doc num:" + doc_num + ", N files:" + n_files);
+        try {
+
+            input_file.readFully(input_arr, 0, index_array.get(doc_num));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Inflater decompresser = new Inflater();
+        decompresser.setInput(input_arr, 0, index_array.get(doc_num));
+        int res_len = 0;
+        try {
+            if ((res_len = decompresser.inflate(result)) > 150000 * 5)
+                System.out.println("decompress error");
+        } catch (DataFormatException e) {
+            e.printStackTrace();
+        }
+        decompresser.end();
+        value = new Text (new String(result, 0, res_len, "UTF-8"));
+        doc_num++;
         return true;
-    };
+    }
 
 
     public void close() throws IOException{
--- hw1/src/main/java/inputformat/DocumentInpFormat2.java
+++ hw1/src/main/java/inputformat/DocumentInpFormat2.java
@@ -1,6 +1,10 @@
 package inputformat;
 
+import com.google.common.io.LittleEndianDataInputStream;
+import org.apache.commons.configuration.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputSplit;
@@ -11,32 +15,77 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.fs.Path;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DocumentInpFormat2 extends FileInputFormat<LongWritable, Text> {
-
+    Integer maxDoc;
+    Configuration conf;
 
     @Override
     public RecordReader<LongWritable, Text> createRecordReader(InputSplit split, TaskAttemptContext context)
-            throws IOException{
-        System.out.println("Here RecordReader I AM HERE!!!");
+            throws IOException {
+        //System.out.println("Here RecordReader I AM HERE!!!");
         DocReader2 reader = new DocReader2();
         reader.initialize(split, context);
         return reader;
     }
 
+    public static final String BYTES_PER_MAP = "mapreduce.input.bmp.bytes_per_map";
+
+    private static List<Integer> read_index(FSDataInputStream index_file) throws IOException {
+        int max_doc = 0;
+        LittleEndianDataInputStream in = new LittleEndianDataInputStream(index_file);
+        List<Integer> al = new ArrayList<>();
+        try {
+            while (true){
+                int val = in.readInt();
+                if (val > max_doc)
+                    max_doc = val;
+                al.add(val);
+            }
+        } catch (EOFException ignored) {
+        }
+        return al;
+    }
+
+
     @Override
-    public List<InputSplit> getSplits(JobContext context) throws IOException {
+    public List<InputSplit> getSplits (JobContext context) throws IOException {
         List<InputSplit> splits = new ArrayList<>();
-        System.out.println("Here getSplits I AM HERE!!!");
+
         for (FileStatus status : listStatus(context)) {
             Path path = status.getPath();
-            splits.add(new FileSplit(path, 0, 0, null));
-        }
+            String index_file = path.getName();
+            if (index_file.substring(index_file.length() - 4).equals(".idx")) {
+                continue;
+            } else {
+                index_file = index_file + ".idx";
+            }
+            FileSystem fs = path.getFileSystem(context.getConfiguration());
+            FSDataInputStream input_index = fs.open(new Path(path.getParent(), index_file));
+            List<Integer> al = read_index(input_index);
 
-        //splits.add(new FileSplit(new Path(1), 0, 0, null));
+            int cur_split = 0;
+            long split_size = 0;
+            long offset = 0;
+            for (Integer cur : al) {
+                split_size += cur;
+                if (cur > maxDoc)
+                    maxDoc = cur;
+                cur_split++;
+                long bytes_num_for_split = context.getConfiguration().getLong(BYTES_PER_MAP, 134217728);
+                if (split_size > bytes_num_for_split) {
+                    splits.add(new FileSplit(path, offset, cur_split, null));
+                    offset += cur_split;
+                    split_size = 0;
+                    cur_split = 0;
+                }
+            }
+            splits.add(new FileSplit(path, offset, cur_split, null));
+        }
         return splits;
-    };
+    }
 }
